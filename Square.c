93fa023b20e2f2efc88043181f5c891c061a3089
Murerwa2063 <kemurerwa@gmail.com>
2022-11-27 05:59:59 -0800

    My first C program edited

--- Square.c
+++ Square.c
@@ -6,7 +6,7 @@ int num1, num2, num3, num4, sum;
 	printf("\n\n\nEnter any four numbers: ")
 	scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
 //calculating the sum of the squares of entered numbers
-sum= (num1*num1) + (num2*num2) + (num3*num3) + (num4*num4)
+sum = (num1*num1) + (num2*num2) + (num3*num3) + (num4*num4)
 
 printf("Answer: %d + %d + %d + %d =  %d ", num1, num2, num3, num4, sum);
 
